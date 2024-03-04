cdbc81879bbd598c5dc2e7dcd44af38a54a4ab9b
Luís Eduardo Batista <luisb@pop-os.localdomain>
2024-03-04 10:00:32 -0300

    Feat: Added gruvbox

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,9 +1,11 @@
 return {
   { "rose-pine/neovim", name = "rose-pine" },
+  { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
+  { "folke/tokyonight.nvim", name = "tokyonight" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "rose-pine",
+      colorscheme = "gruvbox",
     },
   },
 }
