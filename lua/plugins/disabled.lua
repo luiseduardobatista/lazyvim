eb446a2f58fa6ec2f706a9e9e3a298fe24d67c5d
Luís Eduardo Batista <luisb@pop-os.localdomain>
2024-03-04 10:00:46 -0300

    Feat: Updated plugins

--- lua/plugins/disabled.lua
+++ lua/plugins/disabled.lua
@@ -1,5 +1,5 @@
 return {
   { "nvimdev/dashboard-nvim", enabled = false },
-  { "rcarriga/nvim-notify", enabled = false },
+  -- { "rcarriga/nvim-notify", enabled = false },
   { "akinsho/bufferline.nvim", enabled = false },
 }
