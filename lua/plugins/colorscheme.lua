1955744964d410a5b1e06b6db5a8e64d1740ad3c
Luís Eduardo Batista <luiseduardob303@gmail.com>
2024-10-10 09:38:54 -0300

    Feat: Add new colorschemes

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,10 +2,13 @@ return {
   { "rose-pine/neovim", name = "rose-pine" },
   { "ellisonleao/gruvbox.nvim", name = "gruvbox" },
   { "folke/tokyonight.nvim", name = "tokyonight" },
+  { "nyoom-engineering/oxocarbon.nvim", name = "oxocarbon" },
+  { "dasupradyumna/midnight.nvim", name = "midnight" },
+  { "dgox16/oldworld.nvim", name = "oldworld" },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "gruvbox",
+      colorscheme = "catppuccin-macchiato",
     },
   },
 }
