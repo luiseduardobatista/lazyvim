fe5eb4fd61cc108112c756f302543628592c06f6
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-02 06:48:50 -0300

    add hardtime.nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,7 @@
 -- Add any additional keymaps here
 -- vim.keymap.set("n", "<leader>bo", "<cmd>%bd|e#<cr>", { desc = "Close all buffers but the current one" })
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
