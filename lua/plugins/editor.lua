6291696ce21caca0eae51fade214b99b8cf726b2
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-03 08:13:14 -0300

    refactor: remove unnecessary refactoring.nvim opts

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -122,44 +122,12 @@ return {
       {
         "<leader>rp",
         function()
-          require("refactoring").debug.print_var()
+          require("refactoring").debug.print_var({ normal = true })
         end,
         mode = { "n", "x" },
         desc = "Debug Print Variable",
       },
     },
-    opts = {
-      prompt_func_return_type = {
-        go = false,
-        java = false,
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-      },
-      prompt_func_param_type = {
-        go = false,
-        java = false,
-        cpp = false,
-        c = false,
-        h = false,
-        hpp = false,
-        cxx = false,
-      },
-      printf_statements = {},
-      print_var_statements = {},
-      show_success_message = true, -- shows a message with information about the refactor on success
-      -- i.e. [Refactor] Inlined 3 variable occurrences
-    },
-    config = function(_, opts)
-      require("refactoring").setup(opts)
-      if LazyVim.has("telescope.nvim") then
-        LazyVim.on_load("telescope.nvim", function()
-          require("telescope").load_extension("refactoring")
-        end)
-      end
-    end,
   },
   {
     "chaoren/vim-wordmotion",
