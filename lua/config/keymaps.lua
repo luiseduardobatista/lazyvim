243c4665535c7e10671615e004596f7406d7ee54
Luís Eduardo Batista <luiseduardob303@gmail.com>
2025-07-17 09:37:12 -0300

    feat: add go-nvim

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,3 +6,33 @@ vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
+
+vim.api.nvim_create_user_command("DeleteBlankLines", function(opts)
+  local cmd
+  if opts.range > 0 then
+    cmd = string.format("silent %d,%dg/^\\s*$/d", opts.line1, opts.line2)
+  else
+    cmd = "silent %g/^\\s*$/d"
+  end
+
+  -- Executa o comando e mostra uma única mensagem customizada
+  local lines_before = vim.api.nvim_buf_line_count(0)
+  vim.cmd(cmd)
+  local lines_after = vim.api.nvim_buf_line_count(0)
+  local deleted = lines_before - lines_after
+
+  if deleted > 0 then
+    print(string.format("Deleted %d blank line%s", deleted, deleted == 1 and "" or "s"))
+  else
+    print("No blank lines found")
+  end
+end, {
+  desc = "Deleta linhas em branco no buffer ou seleção",
+  range = true,
+})
+
+local wk = require("which-key")
+-- Visual mode - usando o range correto
+wk.add({
+  { "<leader>cd", ":'<,'>DeleteBlankLines<CR>", desc = "Delete Blank Lines", mode = "v" },
+})
